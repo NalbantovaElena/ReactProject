6a0cf67753d26e0b402f0f9c36ffb92f98d3ca56
Elena Nalbantova <48248363+NalbantovaElena@users.noreply.github.com>
2022-04-18 16:12:50 +0300

    Create App.js

---
+++ eShop/src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+import Checkout from './Checkout'
+import {Router, Route, Switch} from 'react-router-dom';
+
+
+
+function App() {
+  return (
+    <div className="App">
+<Router>
+  <Switch>
+    <Route path="/checkout">
+<Header />
+<Checkout />
+    </Route>
+    <Route path="/">
+<Header />
+<Home />
+    </Route>
+  </Switch>
+</Router>
+  </div>
+  );
+}
+
+export default App;
